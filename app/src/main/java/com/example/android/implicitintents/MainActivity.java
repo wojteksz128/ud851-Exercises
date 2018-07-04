2db603ae71ea075e6d55fae084f53e780a370600
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-04 20:06:28 +0200

    Task 4b.2 Open implicitly Google Maps app with querying

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -48,12 +48,18 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        // TODO (5) Store an address in a String
+        // DONE (5) Store an address in a String
+        String addressString = "Łódź, Żniwna";
+        // DONE (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+        final Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+//                .path("0,0")
+                .query(addressString);
+        Uri addressUri = builder.build();
 
-        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
-
-        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        // DONE (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
+//        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(addressUri);
     }
 
     /**
@@ -112,13 +118,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+    // DONE (1) Create a method called showMap with a Uri as the single parameter
+    void showMap(Uri geoLocation) {
 
-        // TODO (3) Set the data of the Intent to the Uri passed into this method
-
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        // Do steps 2 - 4 within the showMap method
+        // DONE (2) Create an Intent with action type, Intent.ACTION_VIEW
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        // DONE (3) Set the data of the Intent to the Uri passed into this method
+        intent.setData(geoLocation);
+        // DONE (4) Verify that this Intent can be launched and then call startActivity
+        if (intent.resolveActivity(getPackageManager())!= null) {
+            startActivity(intent);
+        }
+    }
 
 
 }
