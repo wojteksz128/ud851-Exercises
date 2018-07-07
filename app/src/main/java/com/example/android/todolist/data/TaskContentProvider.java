908fd06ba9fff2c1d82dc977a990961d873a4356
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-08 00:59:49 +0200

    Task 9.6 Content Provider delete() implemented

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -170,14 +170,30 @@ public class TaskContentProvider extends ContentProvider {
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
 
-        // TODO (1) Get access to the database and write URI matching code to recognize a single item
+        // DONE (1) Get access to the database and write URI matching code to recognize a single item
+        final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
+        final int tasksDeleted;
 
-        // TODO (2) Write the code to delete a single row of data
+        // DONE (2) Write the code to delete a single row of data
         // [Hint] Use selections to delete an item by its row ID
+        switch (sUriMatcher.match(uri)) {
+            case TASK_WITH_ID:
+                String id = uri.getPathSegments().get(1);
+                String whereClause = "_id=?";
+                String[] whereArgs = new String[] {id};
+                tasksDeleted = db.delete(TaskContract.TaskEntry.TABLE_NAME, whereClause, whereArgs);
+                break;
 
-        // TODO (3) Notify the resolver of a change and return the number of items deleted
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        // DONE (3) Notify the resolver of a change and return the number of items deleted
+        if (tasksDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return tasksDeleted;
     }
 
 
