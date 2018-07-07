cadb6fcb82fc89ac1316d4eda3f87d49b43cae66
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-08 00:35:30 +0200

    Task 9.5 Content provider query() implemented

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,26 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // DONE (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        Cursor retCursor;
+        // DONE (2) Write URI match code and set a variable to return a Cursor
+        // DONE (3) Query for the tasks directory and write a default case
+        switch (sUriMatcher.match(uri)) {
+            case TASKS:
+                retCursor = db.query(TaskContract.TaskEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
 
-        // TODO (3) Query for the tasks directory and write a default case
+            default:
+                throw new UnsupportedOperationException("Unknown URI: " + uri);
 
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        // DONE (4) Set a notification URI on the Cursor and return that Cursor
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
     }
 
 
