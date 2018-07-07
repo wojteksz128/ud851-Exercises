967100210e2dcc03f4f7fcd7b0c26334f866edcb
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2018-07-08 00:44:56 +0200

    Task 9.4 Add task_with_id option

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -139,6 +139,20 @@ public class TaskContentProvider extends ContentProvider {
                         null,
                         sortOrder);
                 break;
+
+            case TASK_WITH_ID:
+                String id = uri.getPathSegments().get(1);
+                String mSelection = "_id=?";
+                String[] mSelectionArgs = new String[] {id};
+
+                retCursor =  db.query(TABLE_NAME,
+                        projection,
+                        mSelection,
+                        mSelectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
             // Default exception
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
