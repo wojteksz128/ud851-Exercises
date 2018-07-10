4094bf84009438a66d878bf45f4a26bc5fcdf530
Wojciech Szczepaniak <wojciech.szczepaniak@comarch.pl>
2018-07-10 12:00:28 +0200

    Task 10.1 Implement IntentService

--- app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -13,11 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.background.sync;
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+// DONE (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+    //  DONE (10) Create a default constructor that calls super with the name of this class
+    public WaterReminderIntentService() {
+        super(WaterReminderIntentService.class.getName());
+    }
+
+    //  DONE (11) Override onHandleIntent
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+//      DONE (12) Get the action from the Intent that started this Service
+        String action = intent.getAction();
+//      DONE (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+        ReminderTasks.executeTask(this, action);
+
+    }
+
+}
+
+
