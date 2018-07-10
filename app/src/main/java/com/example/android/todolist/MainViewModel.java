efc97bfb414d8c389936923047e23c691602717f
Wojciech Szczepaniak <wojciech.szczepaniak@comarch.pl>
2018-07-10 10:18:37 +0200

    Task 9b.9 Move tasks data into MainViewModel

--- app/src/main/java/com/example/android/todolist/MainViewModel.java
+++ app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -1,11 +1,32 @@
 package com.example.android.todolist;
 
-// TODO (1) make this class extend AndroidViewModel and implement its default constructor
-public class MainViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
 
-    // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
 
-    // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+import java.util.List;
 
-    // TODO (3) Create a getter for the tasks variable
+// DONE (1) make this class extend AndroidViewModel and implement its default constructor
+public class MainViewModel extends AndroidViewModel {
+
+    private final AppDatabase mDb;
+    // DONE (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+    private LiveData<List<TaskEntry>> tasks;
+
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        mDb = AppDatabase.getInstance(application.getApplicationContext());
+        // DONE (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+        tasks = mDb.taskDao().loadAllTasks();
+    }
+
+    // DONE (3) Create a getter for the tasks variable
+    public LiveData<List<TaskEntry>> getTasks() {
+        return tasks;
+    }
 }
